679aaf769ebfa6ae8545fd17c090334afb7fc235
SamanAtashi <milady00126@gmail.com>
2022-01-10 17:35:22 +0300

    Add console functions to main.rb

--- bin/classes/item.rb
+++ bin/classes/item.rb
@@ -1,22 +1,17 @@
 class Item
-    attr_accessor :genre , :author , :source , :label
+  attr_accessor :genre, :author, :source, :label
 
-    def initialize(pd)
-        @id=rand(0..1000)
-        @publish_date= pd
-        @archived=false
-    end
+  def initialize(pd)
+    @id = rand(0..1000)
+    @publish_date = pd
+    @archived = false
+  end
 
+  private def can_be_archived?
+    @publish_date >= 10
+  end
 
-    private def can_be_archived?
-        @publish_date >= 10
-    end
-
-    def move_to_archive
-        can_be_archived? ? @archived=true : nil
-    end
+  def move_to_archive
+    can_be_archived? ? @archived = true : nil
+  end
 end
-
-temp = Item.new(1)
-
-p temp.move_to_archive
