618f9ce317519cd47781806b534ba271821b1708
SamanAtashi <milady00126@gmail.com>
2022-01-10 17:23:33 +0300

    Add archive methods

--- bin/classes/item.rb
+++ bin/classes/item.rb
@@ -2,8 +2,21 @@ class Item
     attr_accessor :genre , :author , :source , :label
 
     def initialize(pd)
-        @id=Random(0..1000)
-        @publish_date=DateTime.new(pd)
+        @id=rand(0..1000)
+        @publish_date= pd
         @archived=false
     end
+
+
+    private def can_be_archived?
+        @publish_date >= 10
+    end
+
+    def move_to_archive
+        can_be_archived? ? @archived=true : nil
+    end
 end
+
+temp = Item.new(1)
+
+p temp.move_to_archive
