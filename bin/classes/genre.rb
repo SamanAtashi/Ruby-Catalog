8d19a85ad61af4f508a44ef05dfd6b2557a720b3
SamanAtashi <milady00126@gmail.com>
2022-01-11 17:12:36 +0300

    Add Genre properties into genre.rb

--- bin/classes/genre.rb
+++ bin/classes/genre.rb
@@ -1,7 +1,12 @@
 
 
 class Genre
+    attr_accessor :name
+    attr_reader :id ,:items
+
     def initialize
-        
+        @id = rand(0..1000)
+        @name=""
+        @items = [] 
     end
 end
