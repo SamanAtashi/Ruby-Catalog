bcee41b394acd7d64b6eda4a35be91d490402fd9
SamanAtashi <milady00126@gmail.com>
2022-01-14 18:41:52 +0300

    Update schema.sql

--- shema.sql
+++ shema.sql
@@ -4,6 +4,26 @@ CREATE TABLE Genre (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE Author(
+  id                 BIGSERIAL PRIMARY KEY,
+  first_name         VARCHAR(100),
+  last_name          VARCHAR(100),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Game(
+  id                BIGSERIAL PRIMARY KEY,
+  author_id         INT,
+  genre_id          INT,
+  last_played_at    DATE,
+  publish_date      DATE,
+  archived          BOOLEAN,
+  label_id          INT,
+  multiplayer       BOOLEAN,
+  PRIMARY KEY (id),
+  FOREIGN KEY (author_id) REFERENCES Author(id),
+);
+
 CREATE TABLE item (
   id INT,
   publish_date DATE,
